afd8267ff3463061ce00fc84c1ca10f29d2ed3ff
admin <admin@YD02-03>
2023-05-22 17:12:49 +0900

    0522

--- BankExample/src/com/yedam/member/MemberService.java
+++ BankExample/src/com/yedam/member/MemberService.java
@@ -1,6 +1,9 @@
 package com.yedam.member;
 
+import java.util.List;
 import java.util.Scanner;
+
+import com.yedam.exe.AccountApp;
 
 public class MemberService {
 	public static Member memberInfo = null;
@@ -26,7 +29,18 @@ public class MemberService {
 		}else {
 			System.out.println("아이디 불일치");
 		}
-				
+	}
+	
+	public void getAccountInfo() {
+		System.out.println("*** 고객 계좌 정보 조회 ***");
 		
+		List<Member> list = MemberDAO.getInstance().getAccountInfo();
+		for(int i=0; i<list.size(); i++) {
+			System.out.println((i+1)+ "번째=====");
+			System.out.println("계좌번호 : " + list.get(i).getAccountId());
+			System.out.println("예금주 : " + list.get(i).getMemberName());
+			System.out.println("잔액 : " + list.get(i).getAccountBalance());
+			System.out.println("등급 : " + (list.get(i).getMemberAuth().equals("N") ? "일반사용자" : "은행원"));
+		}
 	}
 }
